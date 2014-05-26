80a9d6b71d30f504d2f826995ba2d60734958800
テラスカイ1 <dmizoguchi@terrasky.co.jp>
2014-05-26 17:58:51 +0900

    signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
     if @user.save
       sign_in @user
       flash[:success] = "ユーザー登録が完了しました"
-      redirect_to @user
+      redirect_to root_url
     else
       render 'new'
     end
